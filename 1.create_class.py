fdf224d85329abb55630d64e43e3f2088a47389a
hamed <hamed74ghasemi@gmail.com>
2021-05-15 21:37:38 +0430

    define init method

--- 1.create_class.py
+++ 1.create_class.py
@@ -1,5 +1,22 @@
-class BankAccount():
-    pass
+class human():
 
-ba = BankAccount()
-print(type(ba))
+    def __init__(self):
+        
+        print("a new object created")
+        self.w = 74
+        self.h = 1.92
+        self.country = "iran"
+        
+    def walking():
+        pass
+
+    def writing():
+        pass
+
+    def eating():
+        pass
+
+    def speaking():
+
+hasanShamaeizade = human()
+print(type(hasanShamaeizade))
