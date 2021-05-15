769b18cfbea4942f6cecdec515ba1b41316df329
hamed <hamed74ghasemi@gmail.com>
2021-05-15 22:18:05 +0430

    complete example 1

--- 1.create_class.py
+++ 1.create_class.py
@@ -1,23 +1,25 @@
 class human():
 
-    def __init__(self):
+    def __init__(self, name, famil):
         
         print("a new object created")
         self.w = 74
         self.h = 1.92
         self.country = "iran"
+        self.name = name
+        self.famil = famil
         
-    def walking():
+    def walking(self):
         pass
 
-    def writing():
+    def writing(self):
         pass
 
-    def eating():
+    def eating(self):
         pass
 
-    def speaking():
-        pass
+    def introducing(self):
+        print("My name is {} {}".format(self.name, self.famil))
 
-hasanShamaeizade = human()
-print(type(hasanShamaeizade))
+hasanShamaeizade = human("hasan", "shamaizadeh")
+hasanShamaeizade.introducing()
